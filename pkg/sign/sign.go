8ed09b1604b72e4ac5e877c82d514f08da5a8796
Ильшат Гайфуллин <ilshat.gayfullin@sbermarket.ru>
2024-02-05 17:31:58 +0500

    [Iter18] feat: move some packages

---
+++ pkg/sign/sign.go
@@ -0,0 +1,15 @@
+// Package sign contain method for generate sha256 hashed string
+package sign
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+func New(secret string, body []byte) string {
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write(body)
+	signedBody := h.Sum(nil)
+	return hex.EncodeToString(signedBody)
+}
